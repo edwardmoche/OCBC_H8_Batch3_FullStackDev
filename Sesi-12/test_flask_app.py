91a81b673910fe38d4e881d8ed3df9c0047cdbd8
edwardmoche <edwardmoche@yahoo.com>
2022-04-26 13:06:37 +0700

    Hari 5-6

---
+++ Sesi-12/test_flask_app.py
@@ -0,0 +1,13 @@
+import app
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_home(self):
+        result = self.app.get('/')
+        # Make your assertions
